fe921bb3035450c9da53e906ae02b17101924882
paulocruz <pauloluis.f@gmail.com>
2023-02-14 22:21:03 -0300

    em desenvolvimento da aula de http client

--- pacotes/http/main.go
+++ pacotes/http/main.go
@@ -1,19 +1,58 @@
 package main
 
 import (
+	"bytes"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"time"
 )
 
-func main() {
-	req, err := http.Get("https://www.google.com")
+// get
+func main2() {
+	c := http.Client{Timeout: time.Second}
+	resp, err := c.Get("https://www.google.com")
 	if err != nil {
 		panic(err)
 	}
-	defer req.Body.Close()
-	res, err := io.ReadAll(req.Body)
+	defer resp.Body.Close()
+	res, err := io.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
 	println(string(res))
+}
+
+// post
+func main3() {
+	c := http.Client{Timeout: time.Second}
+	jsonVar := bytes.NewBuffer([]byte(`{"name": "paulo"}`))
+	resp, err := c.Post("https://www.google.com", "application/json", jsonVar)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	io.CopyBuffer(os.Stdout, resp.Body, nil)
+}
+
+// http custom
+func main() {
+	c := http.Client{}
+	req, err := http.NewRequest("GET", "https://www.google.com", nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Accept", "application/json")
+	resp, err := c.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
+
 }
